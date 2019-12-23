068e11f88fb1a4819a793b068a9d568e714ad3df
suchoudh <sunil.choudhary@gmail.com>
2019-12-23 20:06:17 +0530

    first add

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title>Welcome Page</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#FFFFFF" link="#0000FF">
+  <tr align="left" valign="top">
+    <td height="346">
+
+        <table width="75%" border="0" cellspacing="10" align="center">
+          <tr align="left" valign="top">
+            <td height="25"><font color="#0000FF" face="Arial, Helve
+tica, sans-serif" size="2">
+              <p align="center"><font size="6" face="Verdana, Arial">
+            <?php
+ echo $SERVER_NAME;
+?>
+        </font></p>
+              </td>
+
+          </tr>
+          <tr>
+            <td colspan="2">
+              <div align="center"><font color="#FF0033" face="Arial, Helvetica,
+sans-serif">The Site is Under Construction
+</a>
+              
+</font></div>
+            </td>
+
+          </tr>
+</table>
+
+
+</body>
+</html>
